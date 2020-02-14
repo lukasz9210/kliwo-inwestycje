49495d39a7a2755833ac81f3465d5ad8553cf99b
Lukasz Komsta <lukasz199286@wp.pl>
2020-02-14 13:21:23 +0100

    Buildings coords

--- src/Buildings.js
+++ src/Buildings.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import {Redirect} from 'react-router-dom'
 import Header from './Header.js'
 import Footer from './Footer.js'
+import $ from 'jquery';
 
 const Buildings = ({match}) => {
     const [building, setBuilding] = useState({})
@@ -11,6 +12,69 @@ const Buildings = ({match}) => {
     const [investmentId, setInvestmentId] = useState(0)
     const [investment, setInvestment] = useState({})
     const [imaheHeight, setImaheHeight] = useState(0)
+    const [dataInCloud, setDataInCloud] = useState({count_m: {free: 0}, count_l: {free: 0}})
+    const [cloudShown, setCloudShown] = useState(false)
+
+
+    const coord = function(xy, orig_size, chngd_size) {
+      const x_scale = chngd_size[0] / orig_size[0];
+      const y_scale = chngd_size[1] / orig_size[1];
+      let coo = [];
+      for (let i = 0; i < xy.length; i += 2) {
+        coo.push(xy[i] * x_scale);
+        coo.push(xy[i+1] * y_scale);
+      }
+      // console.log('coo', coo)
+      return coo;
+    }
+    
+    
+    const parser = function(text, o, ch) {
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(text, "text/xml");
+    
+      let d = xmlDoc.getElementsByTagName('path')[0].getAttribute('d');
+    
+      let arr = d.toUpperCase().replace(/,/g, ' ').replace(/-/g, ' -').split(' ');
+    
+      arr = arr.filter( val => val != '' );
+    
+      let k = 0;
+      while (k < arr.length) {
+        if ((arr[k].length > 1) && (arr[k].match(/[A-Z]/i))) {
+            let tmp = arr[k];
+            arr[k] = arr[k].substr(0, 1);
+            arr.splice(k+1, 0, tmp.substr(1, tmp.length));
+        }
+        k += 1;
+      }
+    
+      let tmp_arr = [];
+      let i = 0;
+      while (i < arr.length) {
+          let tmp = [];
+          if (arr[i].match(/[A-Z]/i)) {
+            let j = i;
+            while (j < arr.length - 1) {
+              tmp.push(arr[j]);
+              //console.log('tmp', tmp)
+              if (arr[j + 1].match(/[A-Z]/i)) {
+                let tmp2 = coord(tmp, o, ch);
+                tmp2 = tmp2.filter( val => !Number.isNaN(val) );
+                tmp_arr.push(arr[i] + tmp2.join(' '));
+                i = j;
+                break;
+              }
+              j += 1
+            }
+          }
+        i += 1;
+      }
+      //console.log('tmp_arr', tmp_arr) 
+      return tmp_arr.join(' ') + ' Z';
+    }
+
+
 
     useEffect(() => {
         fetchBuilding()
@@ -18,13 +82,58 @@ const Buildings = ({match}) => {
 
         setTimeout(() => {
           let ih = document.getElementById('coverImg').height
-        console.log('ih', ih)
+        //console.log('ih', ih)
         setImaheHeight(ih)
-        console.log('imaheHeight', imaheHeight)
+        //console.log('imaheHeight', imaheHeight)
         }, 1000);
 
 
-        console.log(jQuery)
+            //budynek
+    //setTimeOut jest dlatego eby zdąył wrzucić dynamiczne elementy koordynatorówdo kontenera svg
+    setTimeout(() => {
+      //console.log('DONE')
+          //svg container/image
+    let svgContainer = document.getElementById('buildingSvgImg')  //kontener svg
+    const coverImg = document.getElementById('coverImg')  // zdjęcie pod svg
+    
+    //cover img width and height
+    
+    
+    //actual width and height of svg container
+    //console.log('WIDTH SVGCONT', svgContainer)
+    let svgContainerWidth = Math.round(svgContainer.width.baseVal.value / 0.6)
+    let svgContainerHeight = svgContainer.height.baseVal.value
+    //console.log('svgContainerWidth', svgContainerWidth)
+    //console.log('svgContainerHeight', svgContainerHeight)
+    
+    let gElems = svgContainer.querySelectorAll('g')
+    //console.log('gElems', gElems)
+    
+    gElems.forEach((g, i) => {
+      let pathElem = g.querySelector('path')
+      //console.log('path przed', pathElem)
+      let svgText = pathElem.outerHTML
+      //console.log('SVG TEXT', svgText)
+      ///console.log('data for parse', svgText, svgContainerWidth, svgContainerHeight)
+      let newcoords = parser(svgText, [1405, 967], [svgContainerWidth, svgContainerHeight])
+      //console.log('newcoords', newcoords)
+      pathElem.setAttribute('d', newcoords)
+      //console.log('path po', pathElem)
+    })
+    
+    $('path').mouseenter(function() {
+        //jQuery(this).find('path').addClass('svgActive')
+        $(this).addClass('svgActive')
+        console.log("PATH JQUERY", this)
+      })
+    
+      $('path').mouseleave(function() {
+        //jQuery(this).find('path').addClass('svgActive')
+        $(this).removeClass('svgActive')
+      })
+    
+     }, 2000);
+     //koniec budynku
 
 
     }, [])
@@ -140,6 +249,18 @@ const Buildings = ({match}) => {
     }
 
 
+    const mouseOver = data => {
+      console.log('cloud SHOWN', data.count_m.sold)
+      setDataInCloud(data)
+      setCloudShown(true)
+    }
+
+    const mouseLeave = data => {
+      console.log('cloud NOT SHOWN')
+      setCloudShown(false)
+    }
+
+
     return (
         <div className="building">
             <Header />
@@ -173,13 +294,27 @@ const Buildings = ({match}) => {
                 //console.log('l', l)
                 let svg = l.coords
                 //console.log('svg', svg)
-                return <g id={`levelsvg${i + 1}`} onClick={() => {redirectToLevel(l.id)}} dangerouslySetInnerHTML={{__html: svg}}>
+                return <g onMouseOver={() => mouseOver(l)} onMouseLeave={() => mouseLeave()} id={`levelsvg${i + 1}`} onClick={() => {redirectToLevel(l.id)}} dangerouslySetInnerHTML={{__html: svg}}>
                         
                         </g>
               })
             }
             </svg>
             {redirect ? <Redirect to={`/budynek/${redirectId}`} /> : null}
+
+            <div style={{display: cloudShown ? 'block' : 'none'}} className="svg-cloud">
+              <p className="svg-cloud-title">Piętro: {dataInCloud.number}</p>
+              <div className="svg-cloud-boxes flex">
+                <div className="svg-cloud-box w-50">
+                  <span>Dostępne mieszkania</span>
+                  <span>{dataInCloud.count_m.free}</span>
+                </div>
+                <div className="svg-cloud-box w-50">
+                  <span>Dostępne lokale uytkowe</span>
+                  <span>{dataInCloud.count_l.free}</span>
+                </div>
+              </div>
+            </div>
 
             <div className="container">
             <div className="building-details">
--- src/Investments.js
+++ src/Investments.js
@@ -4,6 +4,7 @@ import parse from 'html-react-parser'
 import roletyImg from './images/ico_rolety_zewnetrzne.png'
 import Header from './Header.js'
 import Footer from './Footer.js'
+import $ from 'jquery';
 
 const Test = ({match}) => {
     const [investment, setInvestment] = useState({})
@@ -13,6 +14,65 @@ const Test = ({match}) => {
     const [imaheHeight, setImaheHeight] = useState(0)
     const [cloudShown, setCloudShown] = useState(false)
     const [dataInCloud, setDataInCloud] = useState({})
+
+
+    const coord = function(xy, orig_size, chngd_size) {
+      const x_scale = chngd_size[0] / orig_size[0];
+      const y_scale = chngd_size[1] / orig_size[1];
+      let coo = [];
+      for (let i = 0; i < xy.length; i += 2) {
+        coo.push(xy[i] * x_scale);
+        coo.push(xy[i+1] * y_scale);
+      }
+      // console.log('coo', coo)
+      return coo;
+    }
+    
+    
+    const parser = function(text, o, ch) {
+      let parser = new DOMParser();
+      let xmlDoc = parser.parseFromString(text, "text/xml");
+    
+      let d = xmlDoc.getElementsByTagName('path')[0].getAttribute('d');
+    
+      let arr = d.toUpperCase().replace(/,/g, ' ').replace(/-/g, ' -').split(' ');
+    
+      arr = arr.filter( val => val != '' );
+    
+      let k = 0;
+      while (k < arr.length) {
+        if ((arr[k].length > 1) && (arr[k].match(/[A-Z]/i))) {
+            let tmp = arr[k];
+            arr[k] = arr[k].substr(0, 1);
+            arr.splice(k+1, 0, tmp.substr(1, tmp.length));
+        }
+        k += 1;
+      }
+    
+      let tmp_arr = [];
+      let i = 0;
+      while (i < arr.length) {
+          let tmp = [];
+          if (arr[i].match(/[A-Z]/i)) {
+            let j = i;
+            while (j < arr.length - 1) {
+              tmp.push(arr[j]);
+              //console.log('tmp', tmp)
+              if (arr[j + 1].match(/[A-Z]/i)) {
+                let tmp2 = coord(tmp, o, ch);
+                tmp2 = tmp2.filter( val => !Number.isNaN(val) );
+                tmp_arr.push(arr[i] + tmp2.join(' '));
+                i = j;
+                break;
+              }
+              j += 1
+            }
+          }
+        i += 1;
+      }
+      //console.log('tmp_arr', tmp_arr) 
+      return tmp_arr.join(' ') + ' Z';
+    }
 
     
 
@@ -25,6 +85,44 @@ const Test = ({match}) => {
         setImaheHeight(ih)
         console.log('imaheHeight', imaheHeight)
         }, 1000);
+
+
+            //inwestycja
+    //setTimeOut jest dlatego eby zdąył wrzucić dynamiczne elementy koordynatorówdo kontenera svg
+ setTimeout(() => {
+  //console.log('DONE')
+      //svg container/image
+let svgContainer = document.getElementById('svg1')  //kontener svg
+const coverImg = document.getElementById('coverImg')  // zdjęcie pod svg
+
+//cover img width and height
+let coverImgWidth = coverImg.width
+let coverImgHeight = coverImg.height
+
+//actual width and height of svg container
+let svgContainerWidth = Math.round(svgContainer.width.baseVal.value / 0.6)
+let svgContainerHeight = svgContainer.height.baseVal.value
+//console.log('svgContainerWidth', svgContainerWidth)
+//console.log('svgContainerHeight', svgContainerHeight)
+
+let gElems = svgContainer.querySelectorAll('g')
+//console.log('gElems', gElems)
+
+gElems.forEach((g, i) => {
+  let pathElem = g.querySelector('path')
+  //console.log('path przed', pathElem)
+  let svgText = pathElem.outerHTML
+  //console.log('data for parse', svgText, svgContainerWidth, svgContainerHeight)
+  let newcoords = parser(svgText, [1230, 846], [svgContainerWidth, svgContainerHeight])
+  //console.log('newcoords', newcoords)
+  pathElem.setAttribute('d', newcoords)
+  //console.log('path po', pathElem)
+})
+ }, 2000);
+ //koniec inwestycji
+
+
+
     }, [])
 
     const fetchInvestment = () => {
